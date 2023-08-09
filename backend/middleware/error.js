07bbfbe20c7ad31f1a31c3162b30f38156002d83
AnishMishra1 <anishmishra143@gmail.com>
2023-08-09 15:01:36 +0530

    Error Handling

---
+++ backend/middleware/error.js
@@ -0,0 +1,12 @@
+const ErrorHandler = require("../utils/errorHandler")
+
+module.exports = (err,req,res,next) =>{
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "internal server is error";
+
+    res.status(err.statusCode).json({
+        success:false,
+        message: err.message
+    });
+
+}
