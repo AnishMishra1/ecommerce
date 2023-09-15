1ced8ef2815a1ce8b0742abd39d548123d7e3eb5
AnishMishra1 <anishmishra143@gmail.com>
2023-09-15 10:51:38 +0530

    Updating Css

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -1,48 +1,51 @@
 import React from "react";
 import { ReactNavbar} from "overlay-navbar"
 import logo from "../../../Images/logo.PNG"
+import {FaUser} from "react-icons/fa"
 
 
 
+const options = {
+    burgerColorHover : "#eb4034",
+    logo :logo,
+    logoWidth: "15vmax",
+    navColor1 : "white",
+    logoHoverSize : "10px",
+    logoHoverColor : "#eb4034",
+    link1Text: "Home",
+    link2Text: "Product",
+    link3Text: "Contact",
+    link4Text: "About",
+    link1Url : "/",
+    link2Url : "/product",
+    link3Url : "/contact",
+    link4Url : "/about",
+    link1Size : "1.5vmax",
+    link1Color : "rgba(35,35,35,0.8)",
+    nav1justifyContent: "flex-end",
+    nav2justifyContent: "flex-end",
+    nav3justifyContent: "flex-start",
+    nav4justifyContent: "flex-start",
+    link1colorHover : "#eb4034",
+    link1Margin : "1vmax",
+    profileIconColor : "rgba(35, 35 , 35 ,0.8)",
+    searchIconColor : "rgba(35, 35 , 35 , 0.8)",
+    cartIconColor : "rgba(35, 35, 35, 0.8)",
+    profileIconHover : "#eb4034",
+    cartIconColorHover : "#eb4034",
+    searchIconColorHover :"#eb4034",
+    cartIconMargin : "1vmax",
+    
+    profileIconElement : {FaUser}
+   
+
+}
 
 
 
 
 function Header() {
-    return <ReactNavbar
-    burgerColorHover = "#eb4034"
-    logo ={logo}
-    logoWidth= "15vmax"
-    navColor1 = "white"
-    logoHoverSize = "10px"
-    logoHoverColor = "#eb4034"
-    link1Text= "Home"
-    link2Text= "Product"
-    link3Text= "Contact"
-    link4Text= "About"
-    link1Url = "/"
-    link2Url = "/product"
-    link3Url = "/contact"
-    link4Url = "/about"
-    link1Size = "1.5vmax"
-    link1Color = "rgba(35,35,35,0.8)"
-    nav1justifyContent= "flex-end"
-    nav2justifyContent= "flex-end"
-    nav3justifyContent= "flex-start"
-    nav4justifyContent= "flex-start"
-    link1colorHover = "#eb4034"
-    link1Margin = "1vmax"
-    profileIconColor = "rgba(35, 35 , 35 ,0.8)"
-    searchIconColor = "rgba(35, 35 , 35 , 0.8)"
-    cartIconColor = "rgba(35, 35, 35, 0.8)"
-    profileIconHover = "#eb4034"
-    cartIconColorHover = "#eb4034"
-    searchIconColorHover ="#eb4034"
-    cartIconMargin = "1vmax"
-  
-    
-
-     />
+    return <ReactNavbar {...options} />;
     
 }
 
